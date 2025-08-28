153ffe8dd35a57b6a49934924fa85d66c3ee400c
Maric <maricbrass3@gmail.com>
2025-08-28 19:54:32 +0530

    final commit

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -5,8 +5,3 @@ const appName = 'IVA';
 String userName = 'Person';
 
 late Size mq;
-
-//chat gpt api key (Updated it)
-// String apiKey = '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';
-// String apiKey = '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';
-// String apiKey = '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';
